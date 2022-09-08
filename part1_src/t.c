be0e270fb14d376db2fb78f7c79e673213ec27db
Tanner Blanken <twblanken@gmail.com>
2022-09-08 16:12:32 -0700

    lab1

--- part1_src/t.c
+++ part1_src/t.c
@@ -1,7 +1,34 @@
+typedef unsigned int u32;
+
+char *ctable = "0123456789ABCDEF";
+int BASE = 10;
+
+int rpu(u32 x) 
+{
+	char c;
+	if(x){
+		c = ctable[x %BASE];
+		rpu(x / BASE);
+		putchar(c);
+	}
+}
+int prints(char *s);
+int printu(u32 x)
+{
+	(x==0)? putchar('0') : rpu(x);
+	putchar(' ');
+}
+
+int printx(u32 x);
+int printd(int x);
+int myprintf(char *fmt, ...);
+int main(int argc, char *argv[], char *env[])
+{
+
+}
 
 
-
-// please put your main and code in this file. All contained.
+// put your main and code in this file. All contained.
 // Use the provided mk script to compile your code. You are welcome to change the mk script if you'd like
 // but know that is how we will be batch compiling code using mk on the back end so your code will need to be able
 // to run when that bash script is ran.
--- part2_src/t.c
+++ part2_src/t.c
@@ -1,3 +1,49 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <unistd.h>
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+struct partition {
+	u8 drive; // drive number FD=0, HD=0x80, etc.
+
+	u8 head; // starting head
+	u8 sector; // starting sector
+	u8 cylinder; // starting cylinder
+
+	u8 sys_type; // partition type: NTFS, LINUX, etc.
+	
+	u8 end_head; // end head
+	u8 end_sector; // end sector
+	u8 end_cylinder; // end cylinder
+
+	u32 start_sector; // starting sector counting from 0
+	u32 nr_sectors: // number of sectors in partition
+}
+char *dev = "vdisk";
+int fd;
+
+// read a disk sector into char buf[512]
+int read_sector(int fd, int sector, char *buf)
+{
+	lseek(fd, sector*512, SEEK_SET); // lseek to byte sector*512
+	read(fd, buf, 512); // read 512 bytes into buf[]
+}
+
+int main()
+{
+	struct partition *p;
+	char buf[512];
+
+	fd = open(dev, O_RDONLY); // open dev for READ
+	read(fd, buf, 512); // read 512 bytes of fd into buf[]
+	read_sector(fd, 0, buf); // read in MBR at sector 0
+}
+
 
 
 
